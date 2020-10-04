5d664169461e2c650567a3afb292632184c39c6c
Sean LaFlam <sean.laflam@gmail.com>
2020-10-04 17:19:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let array = [];
+  for (let i=0; i<names.length; i++){
+    array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return array;
+
+}
+
+function countDown(integer){
+  let i = integer + 1
+    while (i > 0){
+      console.log(i - 1);
+      i--;
+    }
+  }
