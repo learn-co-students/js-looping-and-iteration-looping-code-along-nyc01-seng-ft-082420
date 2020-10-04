0cbf6371a7b161743055f20139793067eb529693
jessicasalbert <jessicarsalbert@gmail.com>
2020-10-04 15:39:12 -0400

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+// Code your solutions in this file
+function writeCards(array, event)  {
+    let gifts = [];
+    for (let i = 0; i < array.length; i++) {
+      gifts.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+      }
+      return gifts
+    }
+    
+    function countDown(number) {
+      while (number >= 0) {
+        console.log(number)
+        number --
+      }
+    }
